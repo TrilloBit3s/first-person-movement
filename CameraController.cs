0ea27053db2b3fb5fe835e917b9a9ff40ee71ca5
TrilloBit3s <Trillobit3s@gmail.com>
2024-03-12 03:00:40 -0300

    chaves

--- CameraController.cs
+++ CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamController: MonoBehaviour 
+public class CameraController: MonoBehaviour 
 {
     [SerializeField]
     private GameObject Player;
