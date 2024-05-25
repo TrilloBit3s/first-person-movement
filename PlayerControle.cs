5154997f69ccbbfb4bdb5c6989ba295036b754c7
Trillobit3s indie Games <79748858+TrilloBit3s@users.noreply.github.com>
2024-05-25 16:57:26 -0300

    Update PlayerControle.cs

--- PlayerControle.cs
+++ PlayerControle.cs
@@ -1,37 +1,41 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControle : MonoBehaviour
 {
     public float velocidade = 10f;
-    public Rigidbody rb;
+    private Rigidbody rb;
     public bool EstaNoChao = true;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody não encontrado! Por favor, adicione um Rigidbody ao objeto.");
+        }
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * velocidade;   
-        float vertical = Input.GetAxis("Vertical")  * Time.deltaTime * velocidade;   
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * velocidade;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * velocidade;
 
         transform.Translate(horizontal, 0, vertical);
 
-        if(Input.GetButtonDown("Jump") && EstaNoChao)
+        if (Input.GetButtonDown("Jump") && EstaNoChao && rb != null)
         {
             rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
             EstaNoChao = false;
         }
     }
-    
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Chao")
+        if (collision.gameObject.CompareTag("Chao"))
         {
             EstaNoChao = true;
-        }    
+        }
     }
 }
